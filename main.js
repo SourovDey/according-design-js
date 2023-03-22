4e4c8e89b507458f73d2b13eae234f92c52f3f14
SourovDey <developer.sourovdey@gmail.com>
2023-03-23 03:03:06 +0600

    according v-1

---
+++ main.js
@@ -0,0 +1,98 @@
+
+function hiddenMe(){
+    let hiddenText = document.querySelector(".item__content1");
+    let upArrow = document.querySelector(".up-arrow1")
+    let downArrow = document.querySelector(".down-arrow1")
+
+
+    hiddenText.classList.add("hide-text")
+    upArrow.classList.add("hide-arrow")
+    downArrow.classList.remove("hide-arrow")
+}
+function showMe(){
+    let hiddenText = document.querySelector(".item__content1");
+    let upArrow = document.querySelector(".up-arrow1")
+    let downArrow = document.querySelector(".down-arrow1")
+
+
+    hiddenText.classList.remove("hide-text")
+    upArrow.classList.remove("hide-arrow")
+    downArrow.classList.add("hide-arrow")
+    
+}
+
+
+
+
+
+function hiddenMe2(){
+    let hiddenText2 = document.querySelector(".item__content2");
+    let upArrow2 = document.querySelector(".up-arrow2")
+    let downArrow2 = document.querySelector(".down-arrow2")
+
+
+    hiddenText2.classList.add("hide-text")
+    upArrow2.classList.add("hide-arrow")
+    downArrow2.classList.remove("hide-arrow")
+}
+function showMe2(){
+    let hiddenText2 = document.querySelector(".item__content2");
+    let upArrow2 = document.querySelector(".up-arrow2")
+    let downArrow2 = document.querySelector(".down-arrow2")
+
+
+    hiddenText2.classList.remove("hide-text")
+    upArrow2.classList.remove("hide-arrow")
+    downArrow2.classList.add("hide-arrow")
+    
+}
+
+
+
+
+function hiddenMe3(){
+    let hiddenText3 = document.querySelector(".item__content3");
+    let upArrow3 = document.querySelector(".up-arrow3")
+    let downArrow3 = document.querySelector(".down-arrow3")
+
+
+    hiddenText3.classList.add("hide-text")
+    upArrow3.classList.add("hide-arrow")
+    downArrow3.classList.remove("hide-arrow")
+}
+function showMe3(){
+    let hiddenText3 = document.querySelector(".item__content3");
+    let upArrow3 = document.querySelector(".up-arrow3")
+    let downArrow3 = document.querySelector(".down-arrow3")
+
+
+    hiddenText3.classList.remove("hide-text")
+    upArrow3.classList.remove("hide-arrow")
+    downArrow3.classList.add("hide-arrow")
+    
+}
+
+
+
+
+function hiddenMe4(){
+    let hiddenText4 = document.querySelector(".item__content4");
+    let upArrow4 = document.querySelector(".up-arrow4")
+    let downArrow4 = document.querySelector(".down-arrow4")
+
+
+    hiddenText4.classList.add("hide-text")
+    upArrow4.classList.add("hide-arrow")
+    downArrow4.classList.remove("hide-arrow")
+}
+function showMe4(){
+    let hiddenText4 = document.querySelector(".item__content4");
+    let upArrow4 = document.querySelector(".up-arrow4")
+    let downArrow4 = document.querySelector(".down-arrow4")
+
+
+    hiddenText4.classList.remove("hide-text")
+    upArrow4.classList.remove("hide-arrow")
+    downArrow4.classList.add("hide-arrow")
+    
+}
